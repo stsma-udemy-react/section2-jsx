806a5bce31aa0f07db3f80f650a1457352b13d42
stephensmakilan <stephensmakilan@gmail.com>
2021-02-14 23:58:34 +0800

    Section 2 part 1 commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>submit</button>
+        </div>
+    )
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
